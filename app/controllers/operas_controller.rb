14d5cd82ae4aab45611ceb4639950df040450033
may arden <mayard3n@gmail.com>
2021-05-04 00:02:55 -0700

    builds operas#show and operas#create

--- app/controllers/operas_controller.rb
+++ app/controllers/operas_controller.rb
@@ -2,8 +2,38 @@ class OperasController < ApplicationController
 
     # GET /operas
     def index
-        operas = Opera.all 
-        render json: operas, except: [:created_at, :updated_at]
+        @operas = Opera.all 
+        render json: @operas, except: [:created_at, :updated_at]
     end 
 
+
+    # GET /operas/:id
+    def show
+        @opera = Opera.find(params[:id])
+        render json: @opera 
+    end 
+
+    # POST /operas
+    def create 
+        @opera = Opera.new(opera_params)
+
+        if @opera.save
+            render json: @opera 
+        else 
+            render json: @opera.errors 
+        end 
+
+    end 
+
+    #
+
+    private
+
+    def set_opera
+        @opera = Opera.find(params[:id])
+    end 
+
+    def opera_params
+        params.require(:opera).permit(:title, :director, :description, :img_src)
+    end 
 end
