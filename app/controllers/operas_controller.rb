39a3b0a620e33e6eb17f62e157ae5c96e1a8ef69
may arden <mayard3n@gmail.com>
2021-05-03 23:29:32 -0700

    builds operas#index method

--- app/controllers/operas_controller.rb
+++ app/controllers/operas_controller.rb
@@ -1,2 +1,8 @@
 class OperasController < ApplicationController
+
+    def index
+        operas = Opera.all 
+        render json: operas, include: [:title, :director, :description, :img_src]
+    end 
+
 end
