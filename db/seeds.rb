e8b47a349c42fa8c3b9b40f41ecca977a9aa3ff8
may arden <mayard3n@gmail.com>
2021-05-04 00:10:39 -0700

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Opera.create([
-    { title: "La Boheme", 
-    director: "Franco Zeffirelli", 
-    description: "2019 revival of beloved 1981 Zeffirelli production", 
-    img_src: "https://static01.nyt.com/images/2019/10/31/arts/27boheme3/27boheme3-jumbo.jpg?quality=90&auto=webp"
-    },
+    # { title: "La Boheme", 
+    # director: "Franco Zeffirelli", 
+    # description: "2019 revival of beloved 1981 Zeffirelli production", 
+    # img_src: "https://static01.nyt.com/images/2019/10/31/arts/27boheme3/27boheme3-jumbo.jpg?quality=90&auto=webp"
+    # },
+    # { title: "La Boheme",
+    # director: "Franco Zeffirelli",
+    # description: "2019 revival of beloved 1981 Zeffirelli production",
+    # img_src: "https://static01.nyt.com/images/2018/09/27/arts/27boheme2/merlin_144322842_74306858-a036-4a0a-a8f2-b091cb2ddb3d-jumbo.jpg?quality=90&auto=webp"
+    # },
+    # { title: "Tosca",
+    # director: "David McVicar",
+    # description: "a conservative and traditional staging produced by Peter Gelb after receiving criticism for Luc Bondy's stark staging having been 'gratuitously sordid' and 'messily conceptualized' (The NY Times).",
+    # img_src: "https://static01.nyt.com/images/2018/01/02/arts/02tosca-review2/02tosca-review2-jumbo.jpg?quality=90&auto=webp"
+
+    # }
 ])
