22c48c11ab39a85cebbc02e96047ae03ac6bd46e
may arden <mayard3n@gmail.com>
2021-05-12 14:40:23 -0700

    builds opera class event listeners

--- frontend/src/opera.js
+++ frontend/src/opera.js
@@ -15,6 +15,16 @@ class Opera {
         this.front.id = `front-${this.id}`
         this.front.innerHTML = `<h3 class="title">${this.title}</h3>
                                 <img class="img_src" id="img-${this.id} src="${this.img_src}" alt="${this.title}">`
-    }
+        this.back = document.createElement('div')
+        this.back.id = `back-${this.id}`
+        this.back.hidden = true 
+        this.back.innerHTML = ` <h3 class="director">${this.director}</h3>
+                                <p class="description" id="opera-${this.id}">${this.description}</p>`
+        this.front.addEventListener('mouseover', (e) => this.displayBack(this))
+        this.back.addEventListener('mouseover', (e) => this.displayFront(this))
+        this.element.appendChild(this.front)
+        this.element.appendChild(this.back)
+        Opera.all.push(this)
+        }
 };
 
