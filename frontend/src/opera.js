55ea23122414218abc3a36b343ce79306fcb1e03
may arden <mayard3n@gmail.com>
2021-05-12 14:35:14 -0700

    adds elements/attributes to opera class/object

--- frontend/src/opera.js
+++ frontend/src/opera.js
@@ -1,9 +1,20 @@
 class Opera {
-    constructor(title, director, description) {
-        this.title = title;
-        this.director = director;
-        this.description = description;
-        this.img_src =  img_src; 
+
+    static all = [] 
+
+    constructor({title, director, description}){
+        this.title = title
+        this.director = director
+        this.description = description
+        this.img_src = img_src
+        
+        this.element = document.createElement('div')
+        this.element.setAttribute("class", "box")
+        this.element.id = `opera-${this.id}`
+        this.front = document.createElement('div')
+        this.front.id = `front-${this.id}`
+        this.front.innerHTML = `<h3 class="title">${this.title}</h3>
+                                <img class="img_src" id="img-${this.id} src="${this.img_src}" alt="${this.title}">`
     }
 };
 
