c20c2a40fd36dd183236e56422009c5614f5115b
may arden <mayard3n@gmail.com>
2021-05-12 13:58:57 -0700

    builds submission form and corresponding api class variables

---
+++ frontend/src/operaApi.js
@@ -0,0 +1,4 @@
+const titleInput = document.querySelector('#opera-title')
+const directorInput = document.querySelector('#opera-director')
+const descriptionInput = document.querySelector('#opera-description')
+const imgSrcInput = document.querySelector('#opera-img-src')
