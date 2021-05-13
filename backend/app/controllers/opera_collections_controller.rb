f473c7bb8845a606258f5ac771e53a2cdcc9846f
may arden <mayard3n@gmail.com>
2021-05-13 13:18:13 -0700

    builds opera collection controller functionality

--- backend/app/controllers/opera_collections_controller.rb
+++ backend/app/controllers/opera_collections_controller.rb
@@ -1,2 +1,46 @@
 class OperaCollectionsController < ApplicationController
+    before_action :set_opera_collection, only: [:show, :update, :destroy]
+
+    def index 
+        @opera_collections = OperaCollection.all 
+        render json: @opera_collection.to_json(include: [:opera, :collection]) 
+    end 
+
+    def show 
+        render json: @opera_collection 
+    end 
+
+    def create 
+        @opera_collection = OperaCollection.new(opera_collection_params)
+
+        if @opera_collection.save 
+            render json: @opera_collection, status: :created, location: @opera_collection 
+        else
+            render json: @opera_collection.errors, status: :unprocessable_entity
+        end 
+    end 
+
+    def update 
+        if @opera_collection.update(opera_collection_params)
+            render json: @opera_collection 
+        else
+            render json: @opera_collection.errors, status: :unprocessable_entity
+        end 
+
+    end 
+
+    def destroy 
+        @opera_collection.destroy 
+    end 
+
+    private 
+
+    def set_opera_collection 
+        @opera_collection = Opera.find(params[:id])
+    end 
+
+    def opera_collection_params 
+        params.require(:opera_collection).permit(:collection_id, :opera_id) 
+    end 
+
 end
