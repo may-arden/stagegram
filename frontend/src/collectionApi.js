9585b3eb809561726a3c15510a1677f2de031db2
may arden <mayard3n@gmail.com>
2021-05-13 18:02:40 -0700

    builds configObj method for collection fetch

--- frontend/src/collectionApi.js
+++ frontend/src/collectionApi.js
@@ -3,6 +3,16 @@ const collectionUserInput = document.querySelector("#collection-user")
 
 class CollectionApi {
 
+
+    const configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(collectionInfo)
+    }
+
     fetch(this.baseUrl, configObj)
         .then (r => r.json())
             .then (json => {
